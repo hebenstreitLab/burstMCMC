002ed20aba55e7874d98a5f2f72240f8879f84dc
EdwardsDavid <64012199+EdwardsDavid@users.noreply.github.com>
2023-03-07 10:58:42 +0000

    Add files via upload

--- R/loadPython.R
+++ R/loadPython.R
@@ -1,8 +1,16 @@
 #' @export
 getPython <- function() {
-  reticulate::install_miniconda()
-  reticulate::py_install('scipy')
-  reticulate::py_install('numpy')
+  if (class(try(reticulate::install_miniconda(), silent = T)) != 'try-error') {
+    reticulate::install_miniconda()
+    reticulate::py_install('scipy')
+    reticulate::py_install('numpy')
+  }
+  if (!reticulate::py_module_available('scipy')) {
+    reticulate::py_install('scipy')
+  }
+  if (!reticulate::py_module_available('numpy')) {
+    reticulate::py_install('numpy')
+  }
   reticulate::source_python(file = system.file('python/hypergeomCalculator.py', package = "burstMCMC", mustWork = T), )
   reticulate::source_python(file = system.file('python/f_i_m.py', package = "burstMCMC", mustWork = T))
   calculateHypergeom <<- calculateHypergeom
